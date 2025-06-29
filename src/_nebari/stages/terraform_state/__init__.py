e9552e4824b7b4d7385b12cf695b1006122ec109
Michael Verhulst <michael@terminallabs.com>
2025-06-29 18:26:34 -0500

    Update __init__.py

--- src/_nebari/stages/terraform_state/__init__.py
+++ src/_nebari/stages/terraform_state/__init__.py
@@ -10,61 +10,13 @@ from pydantic import BaseModel, field_validator
 
 from _nebari import utils
 from _nebari.provider import opentofu
-from _nebari.provider.cloud import azure_cloud
 from _nebari.stages.base import NebariTerraformStage
 from _nebari.stages.tf_objects import NebariConfig
 from _nebari.utils import (
-    AZURE_TF_STATE_RESOURCE_GROUP_SUFFIX,
-    construct_azure_resource_group_name,
     modified_environ,
 )
 from nebari import schema
 from nebari.hookspecs import NebariStage, hookimpl
-
-
-class GCPInputVars(schema.Base):
-    name: str
-    namespace: str
-    region: str
-
-
-class AzureInputVars(schema.Base):
-    name: str
-    namespace: str
-    region: str
-    storage_account_postfix: str
-    state_resource_group_name: str
-    tags: Dict[str, str]
-
-    @field_validator("state_resource_group_name")
-    @classmethod
-    def _validate_resource_group_name(cls, value: str) -> str:
-        if value is None:
-            return value
-        length = len(value) + len(AZURE_TF_STATE_RESOURCE_GROUP_SUFFIX)
-        if length < 1 or length > 90:
-            raise ValueError(
-                f"Azure Resource Group name must be between 1 and 90 characters long, when combined with the suffix `{AZURE_TF_STATE_RESOURCE_GROUP_SUFFIX}`."
-            )
-        if not re.match(r"^[\w\-\.\(\)]+$", value):
-            raise ValueError(
-                "Azure Resource Group name can only contain alphanumerics, underscores, parentheses, hyphens, and periods."
-            )
-        if value[-1] == ".":
-            raise ValueError("Azure Resource Group name can't end with a period.")
-
-        return value
-
-    @field_validator("tags")
-    @classmethod
-    def _validate_tags(cls, value: Dict[str, str]) -> Dict[str, str]:
-        return azure_cloud.validate_tags(value)
-
-
-class AWSInputVars(schema.Base):
-    name: str
-    namespace: str
-
 
 @schema.yaml_object(schema.yaml)
 class TerraformStateEnum(str, enum.Enum):
@@ -111,97 +63,14 @@ class TerraformStateStage(NebariTerraformStage):
         return pathlib.Path("stages") / self.name / self.config.provider.value
 
     def state_imports(self) -> List[Tuple[str, str]]:
-        if self.config.provider == schema.ProviderEnum.gcp:
-            return [
-                (
-                    "module.terraform-state.module.gcs.google_storage_bucket.static-site",
-                    f"{self.config.project_name}-{self.config.namespace}-terraform-state",
-                )
-            ]
-        elif self.config.provider == schema.ProviderEnum.azure:
-            subscription_id = os.environ["ARM_SUBSCRIPTION_ID"]
-            resource_name_prefix = f"{self.config.project_name}-{self.config.namespace}"
-            state_resource_group_name = construct_azure_resource_group_name(
-                project_name=self.config.project_name,
-                namespace=self.config.namespace,
-                base_resource_group_name=self.config.azure.resource_group_name,
-                suffix=AZURE_TF_STATE_RESOURCE_GROUP_SUFFIX,
-            )
-            state_resource_name_prefix_safe = resource_name_prefix.replace("-", "")
-            resource_group_url = f"/subscriptions/{subscription_id}/resourceGroups/{state_resource_group_name}"
-
-            return [
-                (
-                    "module.terraform-state.azurerm_resource_group.terraform-state-resource-group",
-                    resource_group_url,
-                ),
-                (
-                    "module.terraform-state.azurerm_storage_account.terraform-state-storage-account",
-                    f"{resource_group_url}/providers/Microsoft.Storage/storageAccounts/{state_resource_name_prefix_safe}{self.config.azure.storage_account_postfix}",
-                ),
-                (
-                    "module.terraform-state.azurerm_storage_container.storage_container",
-                    f"https://{state_resource_name_prefix_safe}{self.config.azure.storage_account_postfix}.blob.core.windows.net/{resource_name_prefix}-state",
-                ),
-            ]
-        elif self.config.provider == schema.ProviderEnum.aws:
-            return [
-                (
-                    "module.terraform-state.aws_s3_bucket.terraform-state",
-                    f"{self.config.project_name}-{self.config.namespace}-terraform-state",
-                ),
-                (
-                    "module.terraform-state.aws_dynamodb_table.terraform-state-lock",
-                    f"{self.config.project_name}-{self.config.namespace}-terraform-state-lock",
-                ),
-            ]
-        else:
-            return []
+        return []
 
     def tf_objects(self) -> List[Dict]:
         resources = [NebariConfig(self.config)]
-        if self.config.provider == schema.ProviderEnum.gcp:
-            return resources + [
-                opentofu.Provider(
-                    "google",
-                    project=self.config.google_cloud_platform.project,
-                    region=self.config.google_cloud_platform.region,
-                ),
-            ]
-        elif self.config.provider == schema.ProviderEnum.aws:
-            return resources + [
-                opentofu.Provider("aws", region=self.config.amazon_web_services.region),
-            ]
-        else:
-            return resources
+        return resources
 
     def input_vars(self, stage_outputs: Dict[str, Dict[str, Any]]):
-        if self.config.provider == schema.ProviderEnum.gcp:
-            return GCPInputVars(
-                name=self.config.project_name,
-                namespace=self.config.namespace,
-                region=self.config.google_cloud_platform.region,
-            ).model_dump()
-        elif self.config.provider == schema.ProviderEnum.aws:
-            return AWSInputVars(
-                name=self.config.project_name,
-                namespace=self.config.namespace,
-            ).model_dump()
-        elif self.config.provider == schema.ProviderEnum.azure:
-            return AzureInputVars(
-                name=self.config.project_name,
-                namespace=self.config.namespace,
-                region=self.config.azure.region,
-                storage_account_postfix=self.config.azure.storage_account_postfix,
-                state_resource_group_name=construct_azure_resource_group_name(
-                    project_name=self.config.project_name,
-                    namespace=self.config.namespace,
-                    base_resource_group_name=self.config.azure.resource_group_name,
-                    suffix=AZURE_TF_STATE_RESOURCE_GROUP_SUFFIX,
-                ),
-                tags=self.config.azure.tags,
-            ).model_dump()
-        elif (
+        if (
             self.config.provider == schema.ProviderEnum.local
             or self.config.provider == schema.ProviderEnum.existing
         ):
