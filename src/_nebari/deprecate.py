0c0fc7c39e7fdc74cff11f1b409462f150e915b6
Michael Verhulst <michael@terminallabs.com>
2025-06-30 01:26:50 -0500

    Update deprecate.py

--- src/_nebari/deprecate.py
+++ src/_nebari/deprecate.py
@@ -1,12 +1,8 @@
 DEPRECATED_FILE_PATHS = [
-    # v0.4 removed in PR #1003 move to stages
     "infrastructure",
     "terraform-state",
-    # v0.4 removed in PR #1068 deprecate some github actions
     ".github/workflows/image-pr.yaml",
     ".github/workflows/image.yaml",
     ".github/workflows/jupyterhub-pr.yaml",
     ".github/workflows/jupyterhub.yaml",
-    # v2024.7.3 renamed misspelled file path
-    "stages/07-kubernetes-services/modules/kubernetes/services/dask-gateway/controler.tf",  # codespell:ignore
 ]
