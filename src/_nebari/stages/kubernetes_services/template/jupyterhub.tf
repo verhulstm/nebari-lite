7fb9a6e59ba7dea31985b7f05c94166c1daa4d9e
Michael Verhulst <michael@terminallabs.com>
2025-06-30 03:03:56 -0500

    Update jupyterhub.tf

--- src/_nebari/stages/kubernetes_services/template/jupyterhub.tf
+++ src/_nebari/stages/kubernetes_services/template/jupyterhub.tf
@@ -194,7 +194,7 @@ module "jupyterhub" {
   jhub-apps-overrides                                = var.jhub-apps-overrides
 
   extra-mounts = {}
-  services = {}
+  services = set("")
 
   general-node-group = var.node_groups.general
   user-node-group    = var.node_groups.user
