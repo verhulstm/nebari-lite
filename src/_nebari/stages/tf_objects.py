8ae5e2dea601174db3cd8e74f28a6bf276f8241b
Michael Verhulst <michael@terminallabs.com>
2025-06-29 23:04:00 -0500

    Update tf_objects.py

--- src/_nebari/stages/tf_objects.py
+++ src/_nebari/stages/tf_objects.py
@@ -1,48 +1,17 @@
 from _nebari.provider.opentofu import Data, Provider, Resource, TerraformBackend
 from _nebari.utils import (
-    AZURE_TF_STATE_RESOURCE_GROUP_SUFFIX,
-    construct_azure_resource_group_name,
     deep_merge,
 )
 from nebari import schema
 
 
 def NebariKubernetesProvider(nebari_config: schema.Main):
-    if nebari_config.provider == "aws":
-        cluster_name = f"{nebari_config.escaped_project_name}-{nebari_config.namespace}"
-        # The AWS provider needs to be added, as we are using aws related resources #1254
-        return deep_merge(
-            Data("aws_eks_cluster", "default", name=cluster_name),
-            Data("aws_eks_cluster_auth", "default", name=cluster_name),
-            Provider("aws", region=nebari_config.amazon_web_services.region),
-            Provider(
-                "kubernetes",
-                host="${data.aws_eks_cluster.default.endpoint}",
-                cluster_ca_certificate="${base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)}",
-                token="${data.aws_eks_cluster_auth.default.token}",
-            ),
-        )
     return Provider(
         "kubernetes",
     )
 
 
 def NebariHelmProvider(nebari_config: schema.Main):
-    if nebari_config.provider == "aws":
-        cluster_name = f"{nebari_config.escaped_project_name}-{nebari_config.namespace}"
-
-        return deep_merge(
-            Data("aws_eks_cluster", "default", name=cluster_name),
-            Data("aws_eks_cluster_auth", "default", name=cluster_name),
-            Provider(
-                "helm",
-                kubernetes=dict(
-                    host="${data.aws_eks_cluster.default.endpoint}",
-                    cluster_ca_certificate="${base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)}",
-                    token="${data.aws_eks_cluster_auth.default.token}",
-                ),
-            ),
-        )
     return Provider("helm")
 
 
@@ -53,35 +22,6 @@ def NebariTerraformState(directory: str, nebari_config: schema.Main):
         return TerraformBackend(
             nebari_config["terraform_state"]["backend"],
             **nebari_config["terraform_state"]["config"],
-        )
-    elif nebari_config.provider == "aws":
-        return TerraformBackend(
-            "s3",
-            bucket=f"{nebari_config.escaped_project_name}-{nebari_config.namespace}-terraform-state",
-            key=f"terraform/{nebari_config.escaped_project_name}-{nebari_config.namespace}/{directory}.tfstate",
-            region=nebari_config.amazon_web_services.region,
-            encrypt=True,
-            dynamodb_table=f"{nebari_config.escaped_project_name}-{nebari_config.namespace}-terraform-state-lock",
-        )
-    elif nebari_config.provider == "gcp":
-        return TerraformBackend(
-            "gcs",
-            bucket=f"{nebari_config.escaped_project_name}-{nebari_config.namespace}-terraform-state",
-            prefix=f"terraform/{nebari_config.escaped_project_name}/{directory}",
-        )
-    elif nebari_config.provider == "azure":
-        return TerraformBackend(
-            "azurerm",
-            resource_group_name=construct_azure_resource_group_name(
-                project_name=nebari_config.project_name,
-                namespace=nebari_config.namespace,
-                base_resource_group_name=nebari_config.azure.resource_group_name,
-                suffix=AZURE_TF_STATE_RESOURCE_GROUP_SUFFIX,
-            ),
-            # storage account must be globally unique
-            storage_account_name=f"{nebari_config.escaped_project_name}{nebari_config.namespace}{nebari_config.azure.storage_account_postfix}",
-            container_name=f"{nebari_config.escaped_project_name}-{nebari_config.namespace}-state",
-            key=f"terraform/{nebari_config.escaped_project_name}-{nebari_config.namespace}/{directory}",
         )
     elif nebari_config.provider == "existing":
         optional_kwargs = {}
