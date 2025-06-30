502a844200bbe22abdb65fc5121e31cfec60d8df
Michael Verhulst <michael@terminallabs.com>
2025-06-29 23:37:46 -0500

    Update main.tf

--- src/_nebari/stages/kubernetes_initialize/template/main.tf
+++ src/_nebari/stages/kubernetes_initialize/template/main.tf
@@ -1,18 +1,13 @@
 module "kubernetes-initialization" {
   source = "./modules/initialization"
-
   namespace = var.environment
   secrets   = []
 }
 
 module "kubernetes-autoscaling" {
-  count = var.cloud_provider == "aws" ? 1 : 0
-
+  count = 0
   source = "./modules/cluster-autoscaler"
-
   namespace = var.environment
-
-  aws_region   = var.aws_region
   cluster-name = local.cluster_name
 }
 
@@ -22,9 +17,7 @@ module "traefik-crds" {
 
 module "nvidia-driver-installer" {
   count = var.gpu_enabled ? 1 : 0
-
   source = "./modules/nvidia-installer"
-
   cloud_provider       = var.cloud_provider
   gpu_enabled          = var.gpu_enabled
   gpu_node_group_names = var.gpu_node_group_names
