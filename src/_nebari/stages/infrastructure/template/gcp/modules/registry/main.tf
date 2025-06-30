4fc1620108f7e7434df4285fb5f0f0c392753878
Michael Verhulst <michael@terminallabs.com>
2025-06-29 20:45:40 -0500

    Delete src/_nebari/stages/infrastructure/template/gcp directory

--- src/_nebari/stages/infrastructure/template/gcp/modules/registry/main.tf
+++
@@ -1,6 +0,0 @@
-resource "google_artifact_registry_repository" "registry" {
-  # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/artifact_registry_repository#argument-reference
-  repository_id = var.repository_id
-  location      = var.location
-  format        = var.format
-}
--- src/_nebari/stages/infrastructure/template/gcp/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "6.14.1"
-    }
-  }
-  required_version = ">= 1.0"
-}
