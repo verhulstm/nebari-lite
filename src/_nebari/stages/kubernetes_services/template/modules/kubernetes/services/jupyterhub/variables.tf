94684aa14ef0656032c0704ae3c2e2015672cae6
Michael Verhulst <michael@terminallabs.com>
2025-06-30 03:22:16 -0500

    Update variables.tf

--- src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/variables.tf
+++ src/_nebari/stages/kubernetes_services/template/modules/kubernetes/services/jupyterhub/variables.tf
@@ -90,7 +90,7 @@ variable "realm_id" {
 variable "services" {
   description = "Set of services that use the jupyterhub api"
   type        = set(string)
-  default     = set("")
+  default     = set([],[])
 }
 
 variable "theme" {
